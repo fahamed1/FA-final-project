a645906f67c6c11c5be1a0e0ea0276d29bd119f4
fahamed1 <fahamed1@fordham.edu>
2024-12-03 20:11:57 -0500

    Adding final project sql

---
+++ final-project.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE `aqi_data`;
+
+CREATE TABLE `user_air_quality` (
+    id INT NOT NULL AUTO_INCREMENT ,
+    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    aqi INT NOT NULL,
+    PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `feedback` (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    feedbackMessage TEXT NOT NULL,
+    PRIMARY KEY (`id`) 
+);
